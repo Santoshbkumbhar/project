673e9f459ee5f137600fd72aeaadb7701ab0b12d
santosh kumbhar <kumbharsantoshb143@gmail.com>
2025-11-22 17:32:32 +0530

    Add initial project file with greeting message
    clean

---
+++ script.js
@@ -0,0 +1,21 @@
+function press(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function backspace() {
+    let text = document.getElementById("display").value;
+    document.getElementById("display").value = text.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+    } catch {
+        document.getElementById("display").value = "Error";
+    }
+}
